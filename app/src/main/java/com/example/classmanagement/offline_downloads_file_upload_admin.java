6be7f35882c1cc4aabb93612bd30b6f4bf70cc6d
hrishi <gawashrishikesh@gmail.com>
2020-11-21 01:13:19 +0530

    Fixed Bug Time Table Selection

---
+++ app/src/main/java/com/example/classmanagement/offline_downloads_file_upload_admin.java
@@ -0,0 +1,14 @@
+package com.example.classmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class offline_downloads_file_upload_admin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_offline_downloads_file_upload_admin);
+    }
+}
