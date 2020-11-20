3ed3c8133c3dfbaaa047b223aabb76b3aeb71028
hrishi <gawashrishikesh@gmail.com>
2020-11-21 00:28:39 +0530

    Added button inside admin for Time table

--- app/src/main/java/com/example/classmanagement/Admin.java
+++ app/src/main/java/com/example/classmanagement/Admin.java
@@ -11,13 +11,21 @@ import android.widget.Toast;
 public class Admin extends AppCompatActivity {
 
     Button testUpdate;
+    Button time_table;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
 
         testUpdate = (Button) findViewById(R.id.testUpdateButton);
-
+        time_table= (Button) findViewById(R.id.time_table);
+        time_table.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),time_table.class);
+                startActivity(intent);
+            }
+        });
         testUpdate.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
